4d666c6a28575769b42051a8b37d1be70ead5578
Om Ingle <omingle7875@gmail.com>
2026-01-22 18:07:14 +0530

    Add IssueService entities and clean tracked artifacts

---
+++ server/IssueService/Models/Assignment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IssueService.Models
+{
+    [Table("ASSIGNMENT", Schema = "issues")]
+    public class Assignment
+    {
+        [Key]
+        public int AssignmentId { get; set; }
+
+        [Required]
+        public int IssueId { get; set; }
+
+        [Required]
+        public int StaffId { get; set; }
+
+        public DateTime AssignedAt { get; set; }
+    }
+}
---
+++ server/IssueService/Models/AuditLog.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IssueService.Models
+{
+    [Table("AUDITLOG", Schema = "issues")]
+    public class AuditLog
+    {
+        [Key]
+        public int LogId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public string Action { get; set; }
+
+        public string EntityType { get; set; }
+
+        public int EntityId { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
