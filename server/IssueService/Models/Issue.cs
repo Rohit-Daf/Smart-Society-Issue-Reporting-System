562b774109531695a4c9fa38efd5994daf724ed8
Rohit-Daf <rohitdaph2@gmail.com>
2026-01-22 17:26:20 +0530

    Added Issue entity (DB-first mapping)

---
+++ server/IssueService/Models/Issue.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IssueService.Models
+{
+    [Table("ISSUE", Schema = "issues")]
+    public class Issue
+    {
+        [Key]
+        public int IssueId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [MaxLength(50)]
+        public string Category { get; set; }
+
+        [MaxLength(20)]
+        public string Priority { get; set; }
+
+        [MaxLength(20)]
+        public string Status { get; set; }
+
+        [MaxLength(20)]
+        public string FlatNo { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public int CreatedBy { get; set; }
+    }
+}
