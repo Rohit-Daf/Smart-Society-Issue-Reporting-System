2528783ad9c2f47bbe78cbe06a472f20571052b0
Rohit-Daf <rohitdaph2@gmail.com>
2026-01-22 17:52:24 +0530

    Added Staff entity for IssueService (DB-first)

---
+++ server/IssueService/Models/Staff.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IssueService.Models
+{
+    [Table("STAFF", Schema = "issues")]
+    public class Staff
+    {
+        [Key]
+        public int StaffId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(50)]
+        public string Role { get; set; }
+
+        [MaxLength(15)]
+        public string Phone { get; set; }
+    }
+}
