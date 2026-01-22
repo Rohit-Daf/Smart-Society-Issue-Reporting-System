78498f7c006b39a93b183f16a4597049c90298a3
Rohit-Daf <rohitdaph2@gmail.com>
2026-01-22 18:22:27 +0530

    Added User entity

---
+++ server/AuthService/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthService.Models
+{
+    [Table("USER", Schema = "auth")]
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(20)]
+        public string FlatNo { get; set; }
+
+        [MaxLength(15)]
+        public string Phone { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Role { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string PasswordHash { get; set; }
+    }
+}
